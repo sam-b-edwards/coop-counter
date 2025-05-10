b627e7b034ee10b5acdcf138c4921247a8b73bad
Sam <itsam_e@redemption-rp.net>
2025-05-10 21:41:45 +1200

    fix issue with send image script

--- camera/send_image.py
+++ camera/send_image.py
@@ -2,18 +2,16 @@ from picamera2 import Picamera2
 import requests
 import time
 
-FILENAME = "/tmp/capture.jpg"
+FILENAME = "/home/sam/capture.jpg"
 SERVER_URL = "http://coopcounter.comdevelopment.com/upload"
 
-# Capture image
-piecam = Picamera2()
+picam = Picamera2()
 picam.configure(picam.create_still_configuration())
 picam.start()
 time.sleep(2)
 picam.capture_file(FILENAME)
+print("Image captured.")
 
-# Send image to server
 with open(FILENAME, "rb") as f:
     response = requests.post(SERVER_URL, files={"file": f})
-
-print("Server response:", response.json())
+    print("Uploaded Server response:", response.json())
